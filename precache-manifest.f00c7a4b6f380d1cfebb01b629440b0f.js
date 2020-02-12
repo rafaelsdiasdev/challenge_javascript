f0b8ff62d8919d1fc94688ca481d03b013b39873
Rafael Dias <rafaels.dias@me.com>
2020-02-12 09:03:08 -0300

    Updates

---
+++ precache-manifest.f00c7a4b6f380d1cfebb01b629440b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b7e136f8704084c03826734410df5a",
+    "url": "/challenge_javascript/index.html"
+  },
+  {
+    "revision": "0f5b0e98a9a5465c5db5",
+    "url": "/challenge_javascript/static/css/main.532b6c07.chunk.css"
+  },
+  {
+    "revision": "77b3147f3adc13215528",
+    "url": "/challenge_javascript/static/js/2.fcf90a57.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/challenge_javascript/static/js/2.fcf90a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5b0e98a9a5465c5db5",
+    "url": "/challenge_javascript/static/js/main.a57e022e.chunk.js"
+  },
+  {
+    "revision": "53901ebfd0d9efa4db29",
+    "url": "/challenge_javascript/static/js/runtime-main.b996347e.js"
+  }
+]);
